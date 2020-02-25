c84047d68c5713a265410f52e867f18fe47a5e99
Jonathan Ho <johnnyho82@gmail.com>
2020-02-25 11:33:36 -0500

    Un-hardcode the machine code

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -56,26 +56,36 @@ class CPU:
     def ram_write(self, mdr, mar):
         self.ram[mar] = mdr
 
-    def load(self):
+    def load(self, progname):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        # # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010,  # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111,  # PRN R0
+        #     0b00000000,
+        #     0b00000001,  # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(progname) as f:
+            for line in f:
+                line = line.split('#')[0]
+                line = line.strip()
+                if line == '':
+                    continue
+                instruction = int(line, 2)
+                self.ram[address] = instruction
+                address += 1
+
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -5,7 +5,14 @@
 import sys
 from cpu import *
 
+
+if len(sys.argv) != 2:
+    print('usage: ls8.py filename')
+    sys.exit(1)
+
+progname = sys.argv[1]
+
 cpu = CPU()
 
-cpu.load()
+cpu.load(progname)
 cpu.run()
